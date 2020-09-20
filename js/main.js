22b6e2198cd8f32731a320ad8fa0d3882f5abeb0
Timothy Wells <mr.timothywells@gmail.com>
2020-09-19 19:23:37 -0700

    19.09.20.0003

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 ﻿var weekNav = document.querySelector(".wk_nav");
 
 var wkllbls = [ "Week 1", "Week 2", "Week 3", "Week 4", "Week 5",];
-var wklnks = ["week_1/week_1.html", "week_2/week_2.html", "week_3/week_3.html", "week_4/week_4.html", "week_5/week_5.html",];
+var wklnks = ["week_1/index.html", "week_2/index.html", "week_3/index.html", "week_4/index.html", "week_5/index.html"];
 
 
 for (i=0; i < wkllbls.length; i++) {
