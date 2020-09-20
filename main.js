bdf57fcb859a22c49d96440a7490be317144e0dc
Timothy Wells <mr.timothywells@gmail.com>
2020-09-19 19:14:15 -0700

    js Update 19.09.20.0001

---
+++ main.js
@@ -0,0 +1,17 @@
+﻿var weekNav = document.querySelector(".wk_nav");
+
+var wkllbls = [ "Week 1", "Week 2", "Week 3", "Week 4", "Week 5",];
+var wklnks = ["week_1/week_1.html", "week_2/week_2.html", "week_3/week_3.html", "week_4/week_4.html", "week_5/week_5.html",];
+
+
+for (i=0; i < wkllbls.length; i++) {
+    var weekLine = document.createElement ("li");
+    var a = document.createElement ("a");
+    var link = document.createTextNode (wkllbls[i]);
+    a.title = wkllbls[i];
+    a.href = wklnks[i];
+
+    a.appendChild(link);
+    weekLine.appendChild(a);
+    weekNav.appendChild(weekLine);
+}
