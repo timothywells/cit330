28f828d29ff2b671bdd5b51325dec07be16e26a8
Timothy Wells <mr.timothywells@gmail.com>
2020-12-05 15:40:11 -0700

    20.12.05.01
    Syncing app

---
+++ wks/Project_2/myApp/main.js
@@ -0,0 +1,13 @@
+//Set API key
+
+//Get info from input
+
+//send request to API
+
+//build and post individual book cards
+
+function volumeCard(){
+    //use innner.HTML to create cards startng with an outer div
+}
+
+//extended portion - setup Oauth 2.0 for personalized information
