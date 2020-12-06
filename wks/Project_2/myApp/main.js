221db8af559202e6b0c124effd6474e0e9be5ecd
Timothy Wells <mr.timothywells@gmail.com>
2020-12-06 15:01:07 -0700

    20.12.06.01
    Got the main components down, just trying to decide on how to show the book informaion.

--- wks/Project_2/myApp/main.js
+++ wks/Project_2/myApp/main.js
@@ -1,13 +1,60 @@
-//Set API key
+function bookSearch(){
+    var search = document.getElementById('inputSearch').value
+    document.getElementById('books').innerHTML = ""
+    console.log(search)
+
+    $.ajax({
+        url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
+        dataType: "json",
+
+        success: function(data) {
+            for(i = 0; i < data.items.length; i++) {
+                books.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>"
+            }
+        },
+        type: 'GET'
+    });
+}
+document.getElementById('searchBTN').addEventListener('click', bookSearch, false)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//API key
+//AIzaSyDdn6yzTBR15sshvZlumi1L_HVSpX3lvgk
 
 //Get info from input
 
 //send request to API
+//https://www.googleapis.com/books/v1/volumes?q=search+terms
 
 //build and post individual book cards
 
-function volumeCard(){
+//function volumeCard(){
     //use innner.HTML to create cards startng with an outer div
-}
+    // <div id="cover"></div>
+    // <ul>
+    //     <li>Title:</li>
+    //     <li>Author:</li>
+    //     <li>Publisher:</li>
+    //     <li>Publish Date:</li>
+    //     <li>Description:</li>
+    // </ul>
+//}
 
 //extended portion - setup Oauth 2.0 for personalized information
