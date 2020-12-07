b4e14826ea62ba03b6d8a10f61bde8ebf9ba1335
Timothy Wells <mr.timothywells@gmail.com>
2020-12-06 17:26:13 -0700

    20.12.06.02
    Getting the DIV working

--- wks/Project_2/myApp/main.js
+++ wks/Project_2/myApp/main.js
@@ -9,27 +9,25 @@ function bookSearch(){
 
         success: function(data) {
             for(i = 0; i < data.items.length; i++) {
-                books.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>"
-            }
+                let result = ""
+                result += "<div class=bookCard>"
+                result += "<img src=" + data.items[i].volumeInfo.imageLinks.smallThumbnail + "/>"
+                result += "<ul>"
+                result += "<li><h2>" + data.items[i].volumeInfo.title + "</h2></li>"
+                result += "<li>Author(s):<ul>" 
+                data.items[i].volumeInfo.authors.forEach(author => {result += "<li>" + author + "</li>" 
+                });
+                result +="</ul></li>"
+                //result += "<li>" +  + "</li>"
+                result += "</ul>"
+                result += "</div>"
+                books.innerHTML += result
+            }      
         },
         type: 'GET'
     });
 }
 document.getElementById('searchBTN').addEventListener('click', bookSearch, false)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -46,7 +44,7 @@ document.getElementById('searchBTN').addEventListener('click', bookSearch, false
 //build and post individual book cards
 
 //function volumeCard(){
-    //use innner.HTML to create cards startng with an outer div
+    //use innner.HTML to create cards startng with an outer div?
     // <div id="cover"></div>
     // <ul>
     //     <li>Title:</li>
