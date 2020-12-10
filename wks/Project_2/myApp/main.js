f4d1b44b750a83ec1906d0ba8332945ef33ffc1a
Timothy Wells <mr.timothywells@gmail.com>
2020-12-10 02:53:46 -0700

    20.12.10.01
    Just a simple update

--- wks/Project_2/myApp/main.js
+++ wks/Project_2/myApp/main.js
@@ -26,6 +26,7 @@ function bookSearch(){
                 //result += "<p>" +  + "</p>"
                 result += "</div></div>"
                 books.innerHTML += result
+                
             }      
         },
         error: function(jqXHR, status, err) {
@@ -41,24 +42,3 @@ document.getElementById('searchBTN').addEventListener('click', bookSearch, false
 
 //API key
 //AIzaSyDdn6yzTBR15sshvZlumi1L_HVSpX3lvgk
-
-//Get info from input
-
-//send request to API
-//https://www.googleapis.com/books/v1/volumes?q=search+terms
-
-//build and post individual book cards
-
-//function volumeCard(){
-    //use innner.HTML to create cards startng with an outer div?
-    // <div id="cover"></div>
-    // <ul>
-    //     <li>Title:</li>
-    //     <li>Author:</li>
-    //     <li>Publisher:</li>
-    //     <li>Publish Date:</li>
-    //     <li>Description:</li>
-    // </ul>
-//}
-
-//extended portion - setup Oauth 2.0 for personalized information
