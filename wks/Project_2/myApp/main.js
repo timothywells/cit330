d2ea6813c700d2d51c5db38e1640a12ce2845002
Timothy Wells <mr.timothywells@gmail.com>
2020-12-12 00:49:07 -0700

    20.12.12.04

--- wks/Project_2/myApp/main.js
+++ wks/Project_2/myApp/main.js
@@ -30,18 +30,18 @@ document.getElementById('searchBTN').addEventListener('click', bookSearch, false
 function forLoop(data) {
     for(i = 0; i <= data.items.length; i++) {
         let result = ""
-        if (data.items[i].volumeInfo.value === undefined) {
-            result += "<p>Information Unavailable</p>"
-        }
+        // if (data.items[i].volumeInfo.value === undefined) {
+        //     result += "<p>Information Unavailable</p>"
+        // }
         result += "<div class=bookCard style={list-style-type: none;}>"
-        result += "<a href=" + data.items[i].volumeInfo.infoLink + "><img class=bookCover src=" + data.items[i].volumeInfo.imageLinks.smallThumbnail + "/></a>"
+        result += "<a href=" + (data.items[i].volumeInfo.infoLink || '') + "><img class=bookCover src=" + (data.items[i].volumeInfo.imageLinks.smallThumbnail || 'Image Unavailable') + "/></a>"
         result += "<div class=bookDetails>"
-        result += "<h3>" + data.items[i].volumeInfo.title + "</h3>"
+        result += "<h3>" + (data.items[i].volumeInfo.title || 'Title Unavailable') + "</h3>"
         result += "<h4>Author(s):</h4>"
         result += "<ul>" 
-            data.items[i].volumeInfo.authors.forEach(author => {result += "<li>" + author + "</li>" })
+            data.items[i].volumeInfo.authors.forEach(author => {result += "<li>" + (author || 'Author Information Unavailable') + "</li>" })
         result += "</ul>"
-        result += "<p id=description>" + data.items[i].volumeInfo.description + "</p>"
+        result += "<p id=description>" + (data.items[i].volumeInfo.description || 'Description Unavailable') + "</p>"
         //result += "<p>" +  + "</p>"
         result += "</div></div>"
         books.innerHTML += result
