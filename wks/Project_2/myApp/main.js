b4b9af8e6ee01afb44213f2ecef528ac776589ef
Timothy Wells <mr.timothywells@gmail.com>
2020-12-12 00:13:16 -0700

    20.12.12.01
    Trying to catch undefined errors

--- wks/Project_2/myApp/main.js
+++ wks/Project_2/myApp/main.js
@@ -29,6 +29,9 @@ document.getElementById('searchBTN').addEventListener('click', bookSearch, false
 
 function forLoop(data) {
     for(i = 0; i <= data.items.length; i++) {
+        if (typeof data.items[i].volumeInfo.value === 'undefined') {
+            result += "<p>Information Unavailable</p>"
+        }
         let result = ""
         result += "<div class=bookCard style={list-style-type: none;}>"
         result += "<a href=" + data.items[i].volumeInfo.infoLink + "><img class=bookCover src=" + data.items[i].volumeInfo.imageLinks.smallThumbnail + "/></a>"
