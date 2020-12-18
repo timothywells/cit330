883cdb832e48bee0999f48617b3e0685cbe68b94
Timothy Wells <mr.timothywells@gmail.com>
2020-12-18 05:23:24 -0700

    20.12.18.01
    Finally finished this final project for my front end class.

--- wks/Project_2/myApp/main.js
+++ wks/Project_2/myApp/main.js
@@ -1,7 +1,6 @@
 function bookSearch(){
     var search = document.getElementById('inputSearch').value
     var select = document.getElementById('searchSelect').value
-    //var APIkey = "AIzaSyDdn6yzTBR15sshvZlumi1L_HVSpX3lvgk"
     document.getElementById('books').innerHTML = ""
     console.log(search)
     if(search == "") {
@@ -9,11 +8,10 @@ function bookSearch(){
         return false
     } else if (select == ""){
         $.ajax({
-            //url: "https://www.googleapis.com/books/v1/volumes?q=" + search + "&maxResults=36&orderBy=relevance&key=" + APIkey,
+            type:"GET",
+            data: {search:search, select: ""},
             url: "https://www.googleapis.com/books/v1/volumes?q=" + search + "&maxResults=36&orderBy=relevance",
-
-            dataType: "json",
-            type: 'GET',
+            dataType: "text",
             success: forLoop,
             error: function() {
                 alert("Please enter informatiion into the search field to find a book");
@@ -21,52 +19,104 @@ function bookSearch(){
         });
     } else {
         $.ajax({
-            //url: "https://www.googleapis.com/books/v1/volumes?q=" + select.select + "&maxResults=36&orderBy=relevance&key=" + APIkey,
-            url: "https://www.googleapis.com/books/v1/volumes?q=" + select.select + "&maxResults=36&orderBy=relevance",
-
-            dataType: "json",
-            type: 'GET',
+            type:"GET",
+            data: {search:search, select:select},
+            url: "https://www.googleapis.com/books/v1/volumes?q=" + select + search + "&maxResults=36&orderBy=relevance",
+            dataType: "text",
             success: forLoop,
             error: function() {
                 alert("Please enter informatiion into the search field to find a book");
             },
         });
     }
+    // if(search == "") {
+    //     alert("Please enter informatiion into the search field to find a book")
+    //     return false
+    // } else if (select == ""){
+    //     $.ajax({
+    //         type: "GET",
+    //         url: "https://www.googleapis.com/books/v1/volumes?q=" + search + "&maxResults=36&orderBy=relevance",
+    //         dataType: "json",
+    //         success: forLoop,
+    //         error: function() {
+    //             alert("Please enter informatiion into the search field to find a book");
+    //         },
+    //     });
+    // } else {
+    //     $.ajax({
+    //         type: "GET",
+    //         url: "https://www.googleapis.com/books/v1/volumes?q=" + select + search + "&maxResults=36&orderBy=relevance",
+    //         dataType: "json",
+    //         success: forLoop,
+    //         error: function() {
+    //             alert("Please enter informatiion into the search field to find a book");
+    //         },
+    //     });
+    // }
 }
-document.getElementById('searchBTN').addEventListener('click', bookSearch, false)
+document.getElementById('searchBTN').addEventListener('click', bookSearch)
 
-function forLoop(data) {
-    for(i = 0; i <= data.items.length; i++) {
+function forLoop(inputdata) {
+    var data = JSON.parse(inputdata)
+    console.log(data)    
+    var i;
+    for(i in data.items) {
         let result = ""
+        //function image() {"try{data.items[i].volumeInfo.imageLinks.thumbnail} catch {'nobook.jpg'}"}
         result += "<div class=bookCard>"
             result += "<div class=row>"
-                result += "<a href=" + (data.items[i].volumeInfo.infoLink || '') + "><img class=bookCover src=" + (data.items[i].volumeInfo.imageLinks.smallThumbnail) + "/></a>"
-                    result += "<div class=bookDetails>"
-                        result += "<h3>" + (data.items[i].volumeInfo.title || 'Title Unavailable') + "</h3>"
-                            result += "<h4>Author(s):</h4>"
-                                result += "<ul>" 
-                                    data.items[i].volumeInfo.authors.forEach(author => {result += "<li>" + (author || 'Author Information Unavailable') + "</li>" })
-                                result += "</ul>"
-                            result += "<p id=description>" + (data.items[i].volumeInfo.description || 'Description Unavailable') + "</p>"
+                result += "<a href=" + (data.items[i].volumeInfo.infoLink || '') + ">"
+                    try {
+                        try {
+                            result += "<img class=bookCover src=" + data.items[i].volumeInfo.imageLinks.thumbnail + "/>"
+                        } catch {
+                            result += "<img class=bookCover src=" + data.items[i].volumeInfo.imageLinks.smallThumbnail + "/>"
+                        }
+                    } catch {
+                        result += "<img class=bookCover src=nobook.jpg/>"
+                    }
+                result += "</a>"
+                result += "<div class=bookDetails>"
+                    result += "<h3 id=title>" + (data.items[i].volumeInfo.title || 'Title Unavailable') + "</h3>"
+                        result += "<h4>Author(s):</h4>"
+                            result += "<ul>"
+                                try {
+                                    data.items[i].volumeInfo.authors.forEach(author => {result += "<li>" + (author) + "</li>"})
+                                } catch {
+                                    result += "<li>Author Unavailable</li>"
+                                }
+                            result += "</ul>"
+                        result += "<p id=description>" + (data.items[i].volumeInfo.description || 'Description Unavailable') + "</p>"
                     result += "</div>"
-            result += "</div>"
+                result += "</div>"
             result += "<div class=links>"
                 result += "<div>"
-                    //result += '<script type=text/javascript src=https://books.google.com/books/previewlib.js></script>'
                     result += '<span id=GBS_Button0><img src=https://books.google.com/intl/en/googlebooks/images/gbs_preview_button1.gif border=0></span>'
-                    result += '<script type=text/javascript>GBS_insertPreviewButtonPopup(\'ISBN:' + data.items[i].volumeInfo.industryIdentifiers[0].identifier + '\');</script>'                        
                 result += "</div>"
                 result += "<div class=linkbtn><a href=" + (data.items[i].volumeInfo.previewLink || alert("Preview Unavailable")) + " target=_blank>Preview</a></div>"
                 result += "<div class=linkbtn><a href=" + (data.items[i].volumeInfo.infoLink || alert("Preview Unavailable")) + " target=_blank>More Info</a></div>"
-                //result += "<div class=linkbtn><a href=" + (data.items[i].volumeInfo.canonicalVolumeLink || alert("Preview Unavailable")) + " target=_blank>Preview</a></div>"
             result += "</div>"        
         result += "</div>"
     books.innerHTML += result
-    }      
+    }
 }
+
+
+// function authors() {
+//     try {
+//         data.items[i].volumeInfo.authors.forEach(author => {result += "<li>" + (author) + "</li>"})
+//     } catch {
+//         return "<li>Author Unavailable</li>"
+//     }
+// }
+
 
 // function APIkey() {
 //     return "AIzaSyDdn6yzTBR15sshvZlumi1L_HVSpX3lvgk"
 // }
 
 //result += "<p>" +  + "</p>"
+
+// data =  JSON.parse(JSON.stringify(data).replace(/:null/gi, ":"""))"
+    //var APIkey = "AIzaSyDdn6yzTBR15sshvZlumi1L_HVSpX3lvgk"
+    //let cleaned = JSON.parse(JSON.stringify(data).replace(/null/gi, "\:\"\""))
